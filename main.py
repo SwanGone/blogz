4a0ef201d44137ed56fe68bb444e5706ed8e67a9
SwanGone <greathairrecords@gmail.com>
2017-06-15 11:01:09 -0500

    Added body to task class

---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:matahari@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+
+class Task(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    blogbody = db.Column(db.String(1600))
+    completed = db.Column(db.Boolean)
+
+    def __init__(self, name):
+        self.name = name
+        self.blogbody = ""
+        self.completed = False
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+    if request.method == 'POST':
+        task_name = request.form['task']
+        new_task = Task(task_name)
+        new_body = request.form['body']
+        new_task.blogbody = new_body
+        db.session.add(new_task)
+        db.session.commit()
+
+    tasks = Task.query.filter_by(completed=False).all()
+    completed_tasks = Task.query.filter_by(completed=True).all()
+    return render_template('add_post.html',title="Bloggo", 
+        tasks=tasks, completed_tasks=completed_tasks)
+
+
+@app.route('/delete-task', methods=['POST'])
+def delete_task():
+
+    task_id = int(request.form['task-id'])
+    task = Task.query.get(task_id)
+    task.completed = True
+    db.session.add(task)
+    db.session.commit()
+
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run()
