3ac38270c60ca689a917ba55ecd853223038a2d8
SwanGone <greathairrecords@gmail.com>
2017-06-18 14:14:17 -0500

    Finished basic requirements for project

--- main.py
+++ main.py
@@ -23,32 +23,40 @@ class Task(db.Model):
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    if request.method == 'GET':
+        return redirect('/blog')
 
-    if request.method == 'POST':
-        task_name = request.form['task']
-        new_task = Task(task_name)
-        new_body = request.form['body']
-        new_task.blogbody = new_body
-        db.session.add(new_task)
-        db.session.commit()
-
-    tasks = Task.query.filter_by(completed=False).all()
-    completed_tasks = Task.query.filter_by(completed=True).all()
-    return render_template('add_post.html',title="Bloggo", 
-        tasks=tasks, completed_tasks=completed_tasks)
-
-
-@app.route('/delete-task', methods=['POST'])
-def delete_task():
-
-    task_id = int(request.form['task-id'])
-    task = Task.query.get(task_id)
-    task.completed = True
-    db.session.add(task)
+    task_name = request.form['task']
+    new_body = request.form['body']
+    if not new_body and not task_name:
+        return render_template('newpost.html', error="Come on. Enter something. I won't judge you!")
+    elif not task_name:
+        return render_template('newpost.html', error="Please enter a title for your blog post")
+    elif not new_body:
+        return render_template('newpost.html', error="Your post has to say something!")
+    new_task = Task(task_name)
+    new_task.blogbody = new_body
+    db.session.add(new_task)
     db.session.commit()
+    task_for_grab = Task.query.filter_by(name=task_name).first()
 
-    return redirect('/')
+    return redirect('/post?task-id=' + str(task_for_grab.id))
 
+
+@app.route('/blog')
+def blog():
+    tasks = Task.query.all()
+    return render_template('blog.html', title="BLOGGO", tasks=tasks)
+
+@app.route('/newpost')
+def newpost():
+    return render_template('newpost.html')
+
+@app.route('/post')
+def post():
+    task_id = int(request.args.get('task-id'))
+    task = Task.query.filter_by(id=task_id).first()
+    return render_template('post.html', task=task)
 
 if __name__ == '__main__':
     app.run()
